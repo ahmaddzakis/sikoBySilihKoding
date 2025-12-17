eeedd5a324f6c14b0789effa5866a2fd06c96920
ahmaddzakis <ahmaddzaki141@gmail.com>
2025-12-17 14:28:03 +0700

    feat: inisialisasi awal

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Livewire\Counter;
+
+Route::get('/counter', Counter::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
